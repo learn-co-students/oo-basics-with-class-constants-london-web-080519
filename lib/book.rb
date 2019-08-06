eff4824c21475de133b025c928d53011f18f0b34
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-08-06 17:04:41 +0100

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,16 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count #set and get these (not genre)
+  attr_reader :title, :genre #get your genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
+  end
+
+  def genre=(genre)
+    @genre = genre #set your genre
+    GENRES << genre #keep track of your genres by shoveling in the genre your setter creates
   end
 
   def turn_page
